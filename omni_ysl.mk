41c485c6a59e8e01144f593b3088795340fffbb0
MiDoNaSR545 <57400341+MiDoNaSR545@users.noreply.github.com>
2020-05-26 19:34:06 +0200

    Update omni_ysl.mk

--- omni_ysl.mk
+++ omni_ysl.mk
@@ -28,3 +28,22 @@ PRODUCT_NAME := omni_ysl
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi S2
 PRODUCT_MANUFACTURER := Xiaomi
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk) -- only for 64bit phones
+
+# Inherit from device
+$(call inherit-product, device/ysl/device.mk) -- path to main device makefile
+
+# Inherit common product files.
+$(call inherit-product, vendor/pb/config/common.mk)
+
+# Set those variables here to overwrite the inherited values.
+BOARD_VENDOR := msm8953
+PRODUCT_BRAND := xiaomi
+PRODUCT_DEVICE := Redmi s2
+PRODUCT_NAME := omni_ysl
+PRODUCT_MANUFACTURER := xiaomi 
+PRODUCT_MODEL := Ysl
+TARGET_VENDOR := msm8953
+
